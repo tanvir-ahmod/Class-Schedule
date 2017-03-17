a75c40687110b580aa271beff6a982cb48d79efa
Shoukhin <shoukhin1993@gmail.com>
2017-03-17 14:48:12 +0600

    updating add routine

--- app/src/main/java/com/example/shoukhin/classroutine/WriteRoutine.java
+++ app/src/main/java/com/example/shoukhin/classroutine/WriteRoutine.java
@@ -10,12 +10,18 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class WriteRoutine extends AppCompatActivity {
 
     private int currentHour, currentMinute;
+    private String key;
 
     EditText day, courseName, courseCode, roomNumber;
     TextView fromTime, toTime;
+
+    private DatabaseReference mFirebaseDatabase;
 
     Button save;
 
@@ -24,24 +30,41 @@ public class WriteRoutine extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_routine);
 
+        initialize();
 
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                String tempDay = day.getText().toString();
+                String tempCourseCode = courseCode.getText().toString();
+                String tempCourseName = courseName.getText().toString();
+                String tempFromTime = fromTime.getText().toString();
+                String temoToTime = toTime.getText().toString();
+                String tempRoomNumber = roomNumber.getText().toString();
+
+                RoutineStructure routine = new RoutineStructure(tempDay, tempCourseName, tempCourseCode, tempFromTime, temoToTime, tempRoomNumber);
+
+                key = mFirebaseDatabase.push().getKey();
+                mFirebaseDatabase.child(tempDay).child(key).setValue(routine);
 
 
             }
         });
     }
 
-    private void initialize()
-    {
+    private void initialize() {
 
         save = (Button) findViewById(R.id.saveRoutineBtn);
 
         courseName = (EditText) findViewById(R.id.addCourseNameEdtx);
         courseCode = (EditText) findViewById(R.id.addCourseCodeEdtx);
         roomNumber = (EditText) findViewById(R.id.addRoomNumEdtx);
+
+        fromTime = (TextView) findViewById(R.id.addfromtimetbx);
+        toTime = (TextView) findViewById(R.id.addtotimetbx);
+
+        mFirebaseDatabase = FirebaseDatabase.getInstance().getReference("routine");
     }
 
     @Override
