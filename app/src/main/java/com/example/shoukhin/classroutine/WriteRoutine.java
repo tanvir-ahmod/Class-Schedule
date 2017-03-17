a03d9c40a51d633ab85b275babf384dead3ce363
Shoukhin <shoukhin1993@gmail.com>
2017-03-17 14:16:46 +0600

    updating add routine

--- app/src/main/java/com/example/shoukhin/classroutine/WriteRoutine.java
+++ app/src/main/java/com/example/shoukhin/classroutine/WriteRoutine.java
@@ -4,16 +4,29 @@ import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TimePicker;
 
 public class WriteRoutine extends AppCompatActivity {
 
     private int currentHour, currentMinute;
 
+    Button save;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_routine);
+
+        save = (Button) findViewById(R.id.saveRoutineBtn);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
     @Override
