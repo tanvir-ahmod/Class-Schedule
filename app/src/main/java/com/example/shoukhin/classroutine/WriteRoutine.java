346b617c39eba3670a1938bca807831cc7e83211
Shoukhin <shoukhin1993@gmail.com>
2017-03-17 14:03:54 +0600

    updating add routine

--- app/src/main/java/com/example/shoukhin/classroutine/WriteRoutine.java
+++ app/src/main/java/com/example/shoukhin/classroutine/WriteRoutine.java
@@ -1,13 +1,38 @@
 package com.example.shoukhin.classroutine;
 
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TimePicker;
 
 public class WriteRoutine extends AppCompatActivity {
+
+    private int currentHour, currentMinute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_routine);
     }
+
+    @Override
+    @Deprecated
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case 0:
+                return new TimePickerDialog(this, mTimeSetListener, currentHour,
+                        currentMinute, false);
+
+            default:
+                break;
+        }
+        return null;
+    }
+
+    private TimePickerDialog.OnTimeSetListener mTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+        }
+    };
 }
