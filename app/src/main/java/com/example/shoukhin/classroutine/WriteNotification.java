bd6b0cab709297fa4c0dd46e99dcdc4a256a02fe
Shoukhin <shoukhin1993@gmail.com>
2017-03-21 16:34:52 +0600

    Notification and pinned post write operation complete

--- app/src/main/java/com/example/shoukhin/classroutine/WriteNotification.java
+++ app/src/main/java/com/example/shoukhin/classroutine/WriteNotification.java
@@ -26,7 +26,7 @@ public class WriteNotification extends AppCompatActivity {
         setContentView(R.layout.activity_write_notification);
 
         text = (EditText) findViewById(R.id.write_noti_edtx);
-        mFirebaseDatabase = FirebaseDatabase.getInstance().getReference("Notification");
+
     }
 
     @Override
@@ -50,13 +50,25 @@ public class WriteNotification extends AppCompatActivity {
             }
 
             NotificationAndPinnedPost notification = new NotificationAndPinnedPost(text.getText().toString());
+
+            mFirebaseDatabase = FirebaseDatabase.getInstance().getReference("Notification");
             String key = mFirebaseDatabase.push().getKey();
             mFirebaseDatabase.child(key).setValue(notification);
 
             //Log.d("tag","saved");
         }
         else if(id == R.id.write_notification_pinned){
-            Log.d("tag", "pinned");
+            if(text.getText().toString().equals(""))
+            {
+                Toast.makeText(WriteNotification.this, "Enter valid data!", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+
+            NotificationAndPinnedPost notification = new NotificationAndPinnedPost(text.getText().toString());
+
+            mFirebaseDatabase = FirebaseDatabase.getInstance().getReference("Notice Board");
+            mFirebaseDatabase.child("notice").setValue(notification);
+            //Log.d("tag", "pinned");
         }
 
         return super.onOptionsItemSelected(item);
