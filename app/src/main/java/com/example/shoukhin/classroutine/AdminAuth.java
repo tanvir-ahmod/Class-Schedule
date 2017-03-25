3c547342dae73b0e6bca924f8e7268abf1d9353a
Shoukhin <shoukhin1993@gmail.com>
2017-03-25 17:51:18 +0600

    primary auth operation successfull

--- app/src/main/java/com/example/shoukhin/classroutine/AdminAuth.java
+++ app/src/main/java/com/example/shoukhin/classroutine/AdminAuth.java
@@ -1,25 +1,85 @@
 package com.example.shoukhin.classroutine;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class AdminAuth extends AppCompatActivity {
 
-    EditText email, password;
+    EditText emailEdtx, passwordEdtx;
 
     Button login;
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_auth);
 
-        email = (EditText) findViewById(R.id.email_edtx);
-        password = (EditText) findViewById(R.id.password_edtx);
+        emailEdtx = (EditText) findViewById(R.id.email_edtx);
+        passwordEdtx = (EditText) findViewById(R.id.password_edtx);
 
         login = (Button) findViewById(R.id.login_button);
+
+        //Get Firebase auth instance
+        auth = FirebaseAuth.getInstance();
+
+        if (auth.getCurrentUser() != null) {
+            startActivity(new Intent(AdminAuth.this, AdminZone.class));
+            finish();
+        }
+
+        //Get Firebase auth instance
+        auth = FirebaseAuth.getInstance();
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailEdtx.getText().toString();
+                final String password = passwordEdtx.getText().toString();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                //authenticate user
+                auth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(AdminAuth.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                if (!task.isSuccessful()) {
+
+                                        Toast.makeText(AdminAuth.this, "Incorrect email or password!", Toast.LENGTH_LONG).show();
+
+                                } else {
+                                    Intent intent = new Intent(AdminAuth.this, AdminZone.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            }
+                        });
+            }
+        });
+
 
     }
 }
