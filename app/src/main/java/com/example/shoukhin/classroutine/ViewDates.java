11b0cedf53bdf690494305f04d3052c397edf850
Shoukhin <shoukhin1993@gmail.com>
2017-08-08 20:23:14 +0600

    Adding view date  Activity

---
+++ app/src/main/java/com/example/shoukhin/classroutine/ViewDates.java
@@ -0,0 +1,13 @@
+package com.example.shoukhin.classroutine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewDates extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_dates);
+    }
+}
